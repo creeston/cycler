fad5308f288ac6d96dd1fc294382c71efec2d212
Chorny, Radzivon <mityy2012@gmail.com>
2026-03-14 22:26:22 +0100

    Fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  base: "/cycler/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/cycler/" : "/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+}));
